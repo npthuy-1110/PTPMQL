cc85a3b9757a43dce355c272cbc23c3b42664c3a
npthuy-1110 <ngphthuy2011@gmail.com>
2025-01-15 21:05:58 +0700

    BTTH

--- PROJECT_NAME/obj/Debug/net9.0/PROJECT_NAME.AssemblyInfo.cs
+++ PROJECT_NAME/obj/Debug/net9.0/PROJECT_NAME.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROJECT_NAME")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a261cbe5e655a2bec55ddca18d07b26d5e8b9fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0753fd52919396f3945eba035fe343e935a0f2a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROJECT_NAME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROJECT_NAME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
